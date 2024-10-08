db49088c19d893c0228287d4cdbb6cda4fc7ea38
SoboliUcho <126193245+SoboliUcho@users.noreply.github.com>
2024-10-08 15:32:48 +0200

    first

---
+++ index.php
@@ -0,0 +1,277 @@
+<?php
+// get info from cnb.cz and return value in NOK or in random currency if NOK is not found in response
+function get_curse($value)
+{
+    // get data from cnb.cz
+    $url = "https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt";
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if ($response === false) {
+        echo 'Curl error: ' . curl_error($ch);
+        exit();
+    }
+
+    // parse data
+    $lines = explode("\n", $response);
+    foreach ($lines as $line) {
+        // find NOK
+        if (strpos($line, "NOK") === false) {
+            continue;
+        }
+        $data = explode("|", $line);
+        $name = $data[3];
+        $multiple = floatval($data[2]);
+        $rate = floatval($data[4]);
+
+        // convert value to NOK
+        $value = ($value * $multiple) / $rate;
+        return $value . " kr ($name)";
+    }
+    // if NOK is not found, return random currency
+    $random = rand(0, count($lines));
+    $line = explode("|", $lines[$random]);
+    $name = $line[3];
+    $multiple = $line[2];
+    $rate = $line[4];
+    $value = ($value * $multiple) / $rate;
+    return $value . " " . $name;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.9.1/font/bootstrap-icons.min.css">
+    <script src="js.js"></script>
+    <title>Ukol</title>
+</head>
+
+<body>
+    <?php
+    // if form is submitted, show order details
+    if (isset($_POST["user"])) {
+        echo "<h1>Objednávka byla odeslána</h1>";
+        $user = json_decode($_POST["user"], true);
+        $products = json_decode($_POST["products"], true);
+        $name = $user["name"];
+        $surname = $user["surname"];
+        $email = $user["email"];
+        $phone = $user["phone"];
+        $street = $user["street"];
+        $streetNumber = $user["streetNumber"];
+        $city = $user["city"];
+        $zip = $user["zip"];
+        $state = $user["state"];
+
+        echo "<div action='post' id='user'>
+        <div class='form_part'>
+            <div>
+                Jméno: <span class='fill'>$name</span>
+            </div>
+            <div>
+                Příjmení: <span class='fill'>$surname</span>
+            </div>
+            <div>
+                Email: <span class='fill'>$email</span>
+            </div>
+            <div>
+                Telefon: <span class='fill'>$phone</span>
+            </div>
+        </div>
+        <div class='form_part'>
+            <div>
+                Ulice: <span class='fill'>$street</span>
+            </div>
+            <div>
+                Číslo popisné: <span class='fill'>$streetNumber</span>
+            </div>
+            <div>
+                Město: <span class='fill'>$city</span>
+            </div>
+            <div>
+                PSČ: <span class='fill'>$zip</span>
+            </div>
+            <div>
+                Stát: <span class='fill'>$state</span>
+            </div>
+        </div>
+    </div>";
+        // print_r($_POST);
+        echo "<h2>Položky objednávky</h2>";
+        echo "<div id='product_form_div'>
+        <div id='product_form'>";
+        $full_price = 0;
+
+        // print products with DPH and without DPH
+        foreach ($products as $product) {
+            echo "<div class='product'>
+            <div class='product_name'>
+                " . $product['name'] . "
+            </div>";
+            if ($product["dph"]) {
+                $full_price += $product["total_price"];
+                $price = $product["price"] / 1.21;
+                $total_price = $product["total_price"] / 1.21;
+            } else {
+                $full_price += $product["total_price"] * 1.21;
+                $price = $product["price"];
+                $total_price = $product["total_price"];
+            }
+            echo "<div class='price'>
+                $price Kč / ks
+            </div>";
+            echo "<div class='quantity'>
+                " . $product['quantity'] . " ks
+            </div>";
+            echo "<div class='total_price'>
+                $total_price Kč  bez DPH
+            </div>";
+            echo "<div>
+                 " . $total_price * 1.21 . " Kč s DPH
+            </div>";
+            echo "</div>";
+        }
+        // print total price in CZK and in NOK
+        echo '<div id="full_price_div">
+        Celková cena: <span id="full_price">' . $full_price . '</span> Kč
+     </div>';
+        echo '<div id="full_price_div">
+        Celková cena: <span id="full_price">' . get_curse($full_price) . '</span>
+     </div>';
+        echo "</div>
+        </div>";
+    } else {
+        // if form is not submitted, show order form
+        echo '<h1>Objednávkový formulář</h1>
+    <form action="post" id="user">
+        <div class="form_part">
+            <div>
+                <label for="name">Jméno:</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div>
+                <label for="surname">Příjmení:</label>
+                <input type="text" name="surname" id="surname">
+            </div>
+            <div>
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="phone">Telefon:</label>
+                <input type="tel" name="phone" id="phone">
+            </div>
+        </div>
+        <div class="form_part">
+            <div>
+                <label for="street">Ulice</label>
+                <input type="text" name="street" id="street">
+            </div>
+            <div>
+                <label for="streetNumber">Číslo popisné:</label>
+                <input type="text" name="streetNumber" id="streetNumber">
+            </div>
+            <div>
+                <label for="city">Město:</label>
+                <input type="text" name="city" id="city">
+            </div>
+            <div>
+                <label for="zip">PSČ:</label>
+                <input type="text" name="zip" id="zip">
+            </div>
+            <div>
+                <label for="state">Stát:</label>
+                <input type="text" name="state" id="state">
+            </div>
+        </div>
+    </form>
+    <div id="product_form_div">
+        <form action="" id="product_form">
+            <!-- <div class="product">
+            <div class="product_name">
+                <label for="product1">Produkt:</label>
+                <input type="text" name="product1" id="product_input1" >
+            </div>
+            <div class="price">
+                <label for="price1">Cena za kus:</label>
+                <input type="number" name="price1" id="price1" >
+            </div>
+            <div class="quantity">
+                <label for="quantity1">Množství:</label>
+                <input type="number" name="quantity1" id="quantity1" >
+            </div>
+            <div class="total_price">
+                <label for="total_price1">Celková cena</label>
+                <input type="number" name="total_price1" id="total_price1" >
+            </div>
+            <button action="button" id="del1" class="del" value="1"><i class="bi bi-trash"></i></button>
+        </div> -->
+        <!-- <div>
+            <label for="dph">DPH</label>
+            <input type="checkbox" name="dph" id="dph1">
+        </div> -->
+        </form>
+        <button id="ad" value="1" type="button">Přidat produkt</button>
+        <div id="full_price_div">
+       Celková cena bez DPH: <span id="full_price">0</span> Kč
+       <br>
+       <br>
+       Celková cena s DPH: <span id="full_price_dph">0</span> Kč
+    </div>
+    </div>
+    <div>
+        <button id="send" type="button">Odeslat</button>
+    </div>';
+        // script for prevent submit form 
+        echo '<script>
+        make_product(0);
+        document.getElementById("product_form").addEventListener("submit", function (e) {
+            e.preventDefault();
+        });';
+        // script for add product
+        echo '
+        document.getElementById("ad").addEventListener("click", function (e) {
+            let buttonValue = parseInt(e.target.value);
+            make_product(buttonValue);
+            // e.target.value = buttonValue + 1;
+        });';
+        // script for prevent submit form by enter
+        echo '
+        document.addEventListener("keydown", function(event) {
+            if (event.key === "Enter" || event.keyCode === 13) {
+                event.preventDefault(); return;
+            }});';
+        // script for send data to server as json in post method
+        echo '
+        document.getElementById("send").addEventListener("click", function (e) {
+            let true_user = validate_user();
+            // let true_user = false;
+            let true_product = validate_product();
+            if (!true_user && !true_product) {
+               return;
+            }
+            let user = get_user();
+            let products = get_products();
+            console.log(user);
+            console.log(products);
+            send_data(user, products);
+        });
+    </script>
+        ';
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ js.js
@@ -0,0 +1,282 @@
+// function to add product to form with all necessary inputs
+function make_product(number) {
+    number++;
+    document.getElementById('ad').value = number;
+    let form = document.getElementById("product_form");
+    let product = document.createElement("div")
+    product.classList.add("product");
+    product.id = `product${number}`;
+    product.innerHTML = `
+        <div class="product_name">
+                <label for="product${number}">Produkt:</label>
+                <input type="text" name="product${number}" id="product_input${number}" >
+            </div>
+            <div class="price">
+                <label for="price${number}">Cena za kus:</label>
+                <input type="number" name="price${number}" id="price${number}" >
+            </div>
+            <div class="quantity">
+                <label for="quantity${number}">Množství:</label>
+                <input type="number" name="quantity${number}" id="quantity${number}" >
+            </div>
+            <div class="total_price">
+                <label for="total_price1">Celková cena</label>
+                <input type="number" name="total_price${number}" id="total_price${number}" > 
+            </div>
+            <div class="dph">
+                <label for="dph">DPH</label>
+                <input type="checkbox" name="dph${number}" id="dph${number}">
+            </div>
+            <button id="del${number}" class="del" value="${number}" type="button"><i class="bi bi-trash"></i></button>
+        </div>
+    `
+    form.appendChild(product);
+    let del = document.getElementById(`del${number}`);
+
+    // add event listener to delete button to remove product from form
+    del.addEventListener("click", function (e) {
+        e.preventDefault();
+        product.remove();
+        calculate_total();
+    })
+    // add event listener to all inputs to calculate total price of product
+    let price = document.getElementById(`price${number}`);
+    let quantity = document.getElementById(`quantity${number}`);
+    let total_price = document.getElementById(`total_price${number}`);
+    let dph = document.getElementById(`dph${number}`);
+
+    price.addEventListener("change", function () {
+        if (price.value && quantity.value && total_price.value) {
+            total_price.value = "";
+        }
+        calculate(number);
+    })
+    quantity.addEventListener("change", function () {
+        if (price.value && quantity.value && total_price.value) {
+            total_price.value = "";
+        }
+        calculate(number);
+    })
+    total_price.addEventListener("change", function () {
+        if (price.value && quantity.value && total_price.value) {
+            price.value = "";
+        }
+        calculate(number);
+    })
+    dph.addEventListener("change", function () {
+        calculate_total();
+    })
+}
+
+// function to calcule missing input in product form
+function calculate(number) {
+    // console.log("calculating");
+    let price = document.getElementById(`price${number}`);
+    let quantity = document.getElementById(`quantity${number}`);
+    let total_price = document.getElementById(`total_price${number}`);
+    // let dph = document.getElementById(`dph${number}`);
+    if (price.value && quantity.value && !total_price.value) {
+        total_price.value = price.value * quantity.value;
+    }
+    if (total_price.value && quantity.value && !price.value) {
+        price.value = total_price.value / quantity.value;
+    }
+    if (total_price.value && price.value && !quantity.value) {
+        quantity.value = total_price.value / price.value;
+    }
+    calculate_total();
+}
+
+// function to calculate total price of all products
+function calculate_total() {
+    let total = document.getElementById("full_price");
+    let total_dph = document.getElementById("full_price_dph");
+    let products = document.getElementsByClassName("product");
+    let sum = 0;
+    for (let product of products) {
+        let total_price = product.querySelector(".total_price").querySelector("input");
+        let dph = product.querySelector(".dph").querySelector("input").checked;
+        if (dph) {
+            sum += Number(total_price.value) / 1.21;
+        } else {
+            sum += Number(total_price.value);
+        }
+    }
+    total.innerHTML = sum.toFixed(2);
+    total_dph.innerHTML = (sum * 1.21).toFixed(2);
+}
+
+// function to check if input in form is filled and if not, highlight it
+function validate_input(input) {
+    let value = input.value;
+    if (value.length < 1) {
+        input.style.border = "1px solid red";
+        input.focus();
+        return false;
+    } else {
+        input.style.border = "revert";
+        return true;
+    }
+}
+
+// function to validate user data are filled in form and in correct format
+function validate_user() {
+    let name = document.getElementById("name");
+    let surname = document.getElementById("surname");
+    let email = document.getElementById("email");
+    let phone = document.getElementById("phone");
+    let street = document.getElementById("street");
+    let streetNumber = document.getElementById("streetNumber");
+    let city = document.getElementById("city");
+    let zip = document.getElementById("zip");
+    let state = document.getElementById("state");
+
+    if (!validate_input(name)) {
+        alert("Vyplňte jméno");
+    } else if (!validate_input(surname)) {
+        alert("Vyplňte příjmení");
+    }
+    else if (!validate_input(email)) {
+        alert("Vyplňte email");
+    }
+    else if (!email.value.includes("@")) {
+        alert("Email není ve správném formátu");
+    }
+    else if (!validate_input(phone)) {
+        alert("Vyplňte telefon");
+    }
+    else if (isNaN(phone.value)) {
+        alert("Telefon musí být číslo");
+    }
+    else if (!validate_input(street)) {
+        alert("Vyplňte ulici");
+    }
+    else if (!validate_input(streetNumber)) {
+        alert("Vyplňte číslo popisné");
+    }
+    else if (!validate_input(city)) {
+        alert("Vyplňte město");
+    }
+    else if (!validate_input(zip)) {
+        alert("Vyplňte PSČ");
+    }
+    else if (isNaN(zip.value)) {
+        alert("PSČ musí být číslo");
+    }
+    else if (!validate_input(state)) {
+        alert("Vyplňte stát");
+    }
+    else {
+        return true;
+    }
+    return false;
+}
+
+// function to validate products data are filled in form and in correct format
+function validate_product() {
+    let products = document.getElementsByClassName("product");
+    if (products.length < 1) {
+        alert("Přidejte alespoň jeden produkt");
+        return false;
+    }
+    let correct = 0;
+    let i = 0;
+    for (let product of products) {
+        // console.log(product);
+        i = product.querySelector(".del").value;
+        let product_name = document.getElementById("product_input" + i);
+        let price = document.getElementById("price" + i);
+        let quantity = document.getElementById("quantity" + i);
+        let total_price = document.getElementById("total_price" + i);
+
+        if (!validate_input(product_name) && (validate_input_product(price) || validate_input_product(quantity) || validate_input_product(total_price))) {
+            alert("Vyplňte název produktu");
+            return false;
+        }
+        if (!validate_input(price) && (validate_input_product(product_name) || validate_input_product(quantity) || validate_input_product(total_price))) {
+            alert("Vyplňte cenu produktu");
+            return false;
+        }
+        if (!validate_input(quantity) && (validate_input_product(price) || validate_input_product(product_name) || validate_input_product(total_price))) {
+            alert("Vyplňte množství produktu");
+            return false;
+        }
+        if (!validate_input(total_price) && (validate_input_product(price) || validate_input_product(quantity) || validate_input_product(product_name))) {
+            alert("Vyplňte celkovou cenu produktu");
+            return false;
+        }
+        if (validate_input_product(product_name) || validate_input_product(price) || validate_input_product(quantity) || validate_input_product(total_price)) {
+            correct++;
+        }
+    }
+    if (correct < 1) {
+        alert("Vyplňte alespoň jednu položku produktu");
+        return false;
+    }
+    return true;
+}
+
+// function to check if input in form is filled
+function validate_input_product(input) {
+    let value = input.value;
+    if (value.length < 1 || isNaN(value) || value == 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// function to get user data from form as object 
+function get_user() {
+    let user = {
+        name: document.getElementById("name").value,
+        surname: document.getElementById("surname").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        street: document.getElementById("street").value,
+        streetNumber: document.getElementById("streetNumber").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value,
+        zip: document.getElementById("zip").value
+    }
+    return user;
+}
+
+// function to get products data from form as array of objects
+function get_products() {
+    let products = [];
+    let product = {};
+    let i = 0;
+    let products_div = document.getElementsByClassName("product");
+    for (let product_div of products_div) {
+        i = product_div.querySelector(".del").value;
+        product = {
+            name: document.getElementById("product_input" + i).value,
+            price: document.getElementById("price" + i).value,
+            quantity: document.getElementById("quantity" + i).value,
+            total_price: document.getElementById("total_price" + i).value,
+            dph: document.getElementById("dph" + i).checked
+        }
+        products.push(product);
+    }
+    return products;
+}
+
+// function to send user and products data to server
+function send_data(user, products) {
+    let form = document.createElement("form");
+    form.method = "POST";
+    let user_input = document.createElement("input");
+    user_input.type = "hidden";
+    user_input.name = "user";
+    user_input.value = JSON.stringify(user);
+    form.appendChild(user_input);
+    let products_input = document.createElement("input");
+    products_input.type = "hidden";
+    products_input.name = "products";
+    products_input.value = JSON.stringify(products);
+    form.appendChild(products_input);
+
+    document.body.appendChild(form);
+    form.submit();
+}
